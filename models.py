2a2974c4ac9bc37873391a23453bc6622fb8c638
Eric Jostad <eric.jostad@outlook.com>
2026-03-26 12:21:58 -0700

    Added WatchList model to allow for transfer of data to new watch_list table in gundam.db database

--- models.py
+++ models.py
@@ -9,9 +9,23 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.utcnow)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
+
+
+class WatchList(db.Model):
+    __tablename__ = 'watch_list'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    media_name = db.Column(db.String, nullable=False)
+    watched = db.Column(db.Boolean, default=False)
+    rating = db.Column(db.Integer, nullable=True)
+    thoughts = db.Column(db.String, nullable=True)
+
+    user = db.relationship("User", backref="watch_list")
